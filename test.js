ae5c891fc829d1c0df239a5a552c5cb6c05f4ea3
Valentin <walik91@gmail.com>
2020-04-01 14:22:44 +0300

    Create test.js

---
+++ test.js
@@ -0,0 +1,156 @@
+$('.load-more').remove();
+
+$('.page-title').replaceWith('<article class="single-post" id="article" data-target="147">\n' +
+  '\t\t\t\t    <header class="post-header">\n' +
+  '\t\t\t\t<a class="entry-category" href="Inspiratie">Actual</a>\n' +
+  '\t\t\t\t\n' +
+  '\t\t\t\t<h1 class="post-title">Guvernul va plăti 50% din salariu tuturor celor care lucrează de la distanță</h1>\n' +
+  '\t\t\t\t<ul class="entry-meta post-meta">\n' +
+  '\t\t\t\t\t<li class="entry-meta-author"><img src="cdn/p/authors/small/adriana-faureanu.jpg" alt="Adriana Făureanu">Adriana Făureanu</li>\n' +
+  '\t\t\t\t\t<li class="entry-meta-date">\n' +
+  '\t\t\t\t\t\t<i class="icon-calendar"></i>02 Aprilie 2020\n' +
+  '\t\t\t\t\t</li>\n' +
+  '\t\t\t\t\t<li class="entry-meta-hour">\n' +
+  '\t\t\t\t\t\t<i class="icon-time"></i> 13:14\n' +
+  '\t\t\t\t\t</li>\n' +
+  '\t\t\t\t</ul>\n' +
+  '\t\t\t</header>\n' +
+  '\t\t\t<figure>\n' +
+  '\t\t\t\t<img class="lazy featimg loaded" src="https://i.simpalsmedia.com/point.md/news/370x194/beb556ff98d7be34eba233ee2d150c67.jpg" alt="Ruslan Cojocaru - Viziunea față de crize, educație financiară, Tucano Coffee, Oliva Verde și NOTA lui DE PLATĂ (VIDEO)" data-was-processed="true">\n' +
+  '\t\t\t\t<figcaption>\n' +
+  '\t\t\t\t    \n' +
+  '\t\t\t\t</figcaption>\n' +
+  '\t\t\t</figure>\n' +
+  '\t\t\t<div class="post-content">\n' +
+  '              <ul class="post-sharing flexer">\n' +
+  '                <li>\n' +
+  '                  <div class="fb-share-button fb_iframe_widget" data-href="https://agora.md/stiri/69073/ruslan-cojocaruviziunea-fata-de-crize-educatie-financiara-tucano-coffee-oliva-verde-si-nota-lui-de-platA-video" data-layout="button" data-size="small" data-mobile-iframe="true" fb-xfbml-state="rendered" fb-iframe-plugin-query="app_id=334679903395197&amp;container_width=215&amp;href=https://agora.md/stiri/69073/ruslan-cojocaruviziunea-fata-de-crize-educatie-financiara-tucano-coffee-oliva-verde-si-nota-lui-de-platA-video&amp;layout=button_count&amp;locale=en_US&amp;mobile_iframe=true&amp;sdk=joey&amp;size=small">\n' +
+  '                    \n' +
+  '                    <span style="vertical-align: bottom; width: 69px; height: 20px;">\n' +
+  '                        <iframe name="f9f55f9a0b9a4" width="1000px" height="1000px" title="fb:share_button Facebook Social Plugin" frameborder="0" allowtransparency="true" allowfullscreen="true" scrolling="no" allow="encrypted-media" src="https://www.facebook.com/v2.3/plugins/share_button.php?app_id=334679903395197&amp;channel=https%3A%2F%2Fstaticxx.facebook.com%2Fconnect%2Fxd_arbiter.php%3Fversion%3D44%23cb%3Df107ecf07db6cac%26domain%3Dagora.md%26origin%3Dhttps%253A%252F%252Fagora.md%252Ff2d40a19a4d2888%26relation%3Dparent.parent&amp;container_width=215&amp;href=https://agora.md/stiri/69073/ruslan-cojocaruviziunea-fata-de-crize-educatie-financiara-tucano-coffee-oliva-verde-si-nota-lui-de-platA-video&amp;layout=button&amp;locale=en_US&amp;mobile_iframe=true&amp;sdk=joey&amp;size=small" style="border: none; visibility: visible; width: 69px; height: 20px;" class=""></iframe></span></div>\n' +
+  '                </li>\n' +
+  '                <li>\n' +
+  '                  <iframe src="https://platform.twitter.com/widgets/tweet_button.html?size=s&amp;url=https://agora.md/stiri/69073/ruslan-cojocaruviziunea-fata-de-crize-educatie-financiara-tucano-coffee-oliva-verde-si-nota-lui-de-platA-video&amp;related=twitterapi%2Ctwitter&amp;text=Ruslan Cojocaru - Viziunea față de crize, educație financiară, Tucano Coffee, Oliva Verde și NOTA lui DE PLATĂ (VIDEO)" width="65" height="20" title="Twitter Tweet Button" style="border: 0; overflow: hidden;" scrolling="no">\n' +
+  '                  </iframe>\n' +
+  '                </li>\n' +
+  '              </ul>\n' +
+  '              <p class="preamble"></p><p class="preamble">Situația cu coronavirus se agravează, iar pentru a evita o creștere mai rapidă a numărului de pacienți, guvernul a decis să stimuleze munca de acasă.</p>\n' +
+  '              \n' +
+  '              \n' +
+  '              <p>Astfel, guvernul va plăti 50% din salariu tuturor celor care lucrează de la distanță. În același timp, angajatorul are dreptul să plătească restul de 50%, iar angajatul va primi astfel tariful integral sau, poate salva 100% din salariu, iar angajatul va primi în cele din urmă 1,5 rate</p><p>Mulți reprezentanți ai afacerilor și-au dat seama că este necesar să comutați la modul de operare de la distanță cât mai mult posibil, dar nu toată lumea decide acest lucru. Astfel, guvernul a decis să încurajeze angajatorii să facă un astfel de pas.</p>\n' +
+  '\n' +
+  '              <p>Trebuie menționat că acest lucru se aplică numai celor care vor începe ACUM să treacă la lucrările la distanță. Cei care lucrează deja de la distanță nu vor primi o plata aditionala.</p><p>Reamintim că situația cu coronavirus pizdetc se agravează în fiecare zi. Conform prognozelor OMS, până la jumătatea lunii aprilie, numărul pacienților va depăși 28 de mii de persoane.</p><div class="inline-news advertoriale">\n' +
+  '    <h6>Vezi și aceste știri</h6><div class="grid-view cols-3"><article>\n' +
+  '                            <figure>\n' +
+  '                              <img src="cdn/p//news/medium/nota-de-plata-a-designerei-olga-blanc-despre-bani-dezvoltare-personala-barbatul-care-o-sustine-si-succesul-brandului-oblanc-video-147105-1583335171.jpg" alt="Nota de Plată a designerei Olga Blanc. Despre bani, dezvoltare personală, bărbatul care o susține și succesul brandului O\'Blanc (VIDEO)">\n' +
+  '                            </figure>\n' +
+  '                            <header>\n' +
+  '                              <h2 class="title">\n' +
+  '                                Nota de Plată a designerei Olga Blanc. Despre bani, dezvoltare personală, bărbatul care o susține și succesul brandului O\'Blanc (VIDEO)\n' +
+  '                              </h2>\n' +
+  '                            </header>\n' +
+  '                            <a href="https://agora.md/stiri/67745/nota-de-plata-a-designerei-olga-blanc-despre-bani-dezvoltare-personala-barbatul-care-o-sustine-si-succesul-brandului-oblanc-video" class="article-link"></a>\n' +
+  '                          </article><article>\n' +
+  '                            <figure>\n' +
+  '                              <img src="cdn/p//news/medium/nota-de-plata-a-lui-alexandru-luchianov-despre-profitul-vinariei-et-cetera-bani-pierduti-dezvoltare-personala-si-cum-e-sa-faci-business-cu-fratele-146266.jpg" alt="Nota de plată a lui Alexandru Luchianov. Despre profitul vinăriei Et Cetera, bani pierduți, dezvoltare personală și cum e să faci business cu fratele (VIDEO) \n' +
+  '\n' +
+  '">\n' +
+  '                            </figure>\n' +
+  '                            <header>\n' +
+  '                              <h2 class="title">\n' +
+  '                                Nota de plată a lui Alexandru Luchianov. Despre profitul vinăriei Et Cetera, bani pierduți, dezvoltare personală și cum e să faci business cu fratele (VIDEO) \n' +
+  '\n' +
+  '\n' +
+  '                              </h2>\n' +
+  '                            </header>\n' +
+  '                            <a href="https://agora.md/actual/nota-de-plata-a-lui-alexandru-luchianov-despre-profitul-vinariei-et-cetera-bani-pierduti-dezvoltare-personala-si-cum-e-sa-faci-business-cu-fratele-video" class="article-link"></a>\n' +
+  '                          </article><article>\n' +
+  '                            <figure>\n' +
+  '                              <img src="cdn/p//news/medium/-Nota-de-Plata-un-nou-proiect-video-AGORA-Te-provocam-sa-inveti-de-la-cei-mai-buni-PROMO--66712-1581339001.jpg" alt="„Nota de Plată”, un nou proiect video AGORA. Te provocăm să înveți de la cei mai buni (PROMO)">\n' +
+  '                            </figure>\n' +
+  '                            <header>\n' +
+  '                              <h2 class="title">\n' +
+  '                                „Nota de Plată”, un nou proiect video AGORA. Te provocăm să înveți de la cei mai buni (PROMO)\n' +
+  '                              </h2>\n' +
+  '                            </header>\n' +
+  '                            <a href="https://agora.md/stiri/66712/nota-de-plata--un-nou-proiect-video-agora--te-provocam-sa-inveti-de-la-cei-mai-buni-promo" class="article-link"></a>\n' +
+  '                          </article></div></div>\n' +
+  '\t\t\t\t\t\t<div class="inline-news advertoriale">\n' +
+  '\t\t\t\t\t\t\t<h6>Advertoriale</h6>\n' +
+  '\t\t\t\t\t\t\t<div class="grid-view cols-3">\n' +
+  '\t\t\t\t\t\t\t    <article>\n' +
+  '                        <figure>\n' +
+  '                          <img class="lazy" src="files/img/default-lazy.gif" data-src="cdn/p//news/medium/solidaritatea-imbraca-haina-bunuluisimt-un-brand-de-haine-si-un-ong-vor-imparti-masti-de-protectie-batranilor-148489-1585731604.jpg" alt="Solidaritatea îmbracă haina bunului-simț: un brand de haine și un ONG vor împărți măști de protecție bătrânilor">\n' +
+  '                        </figure>\n' +
+  '                        <header>\n' +
+  '                          <h2 class="title">\n' +
+  '                            Solidaritatea îmbracă haina bunului-simț: un brand de haine și un ONG vor împărți măști de protecție bătrânilor\n' +
+  '                          </h2>\n' +
+  '                        </header>\n' +
+  '                        <a href="https://agora.md/stiri/69104/solidaritatea-imbraca-haina-bunuluisimt-un-brand-de-haine-si-un-ong-vor-imparti-masti-de-protectie-batranilor" class="article-link"></a>\n' +
+  '                      </article><article>\n' +
+  '                        <figure>\n' +
+  '                          <img class="lazy" src="files/img/default-lazy.gif" data-src="cdn/p//news/medium/depozitele-care-expira-in-aprilie-–-prelungite-automat-de-maib-148481-1585727879.jpg" alt="MAIB anunță prelungire automată pentru depozitele care expiră în aprilie ">\n' +
+  '                        </figure>\n' +
+  '                        <header>\n' +
+  '                          <h2 class="title">\n' +
+  '                            MAIB anunță prelungire automată pentru depozitele care expiră în aprilie \n' +
+  '                          </h2>\n' +
+  '                        </header>\n' +
+  '                        <a href="https://agora.md/stiri/69096/maib-anunta-prelungire-automata-pentru-depozitele-care-expira-in-aprilie" class="article-link"></a>\n' +
+  '                      </article><article>\n' +
+  '                        <figure>\n' +
+  '                          <img class="lazy" src="files/img/default-lazy.gif" data-src="cdn/p//news/medium/endava-adera-la-initiativele-civice-destinate-medicilor-din-moldova-148436-1585654680.jpg" alt="Endava aderă la inițiativele civice destinate suportului medicilor din țara noastră">\n' +
+  '                        </figure>\n' +
+  '                        <header>\n' +
+  '                          <h2 class="title">\n' +
+  '                            Endava aderă la inițiativele civice destinate suportului medicilor din țara noastră\n' +
+  '                          </h2>\n' +
+  '                        </header>\n' +
+  '                        <a href="https://agora.md/stiri/69051/endava-adera-la-initiativele-civice-destinate-suportului-medicilor-din-tara-noastra" class="article-link"></a>\n' +
+  '                      </article>\n' +
+  '\t\t\t\t\t\t\t\t\n' +
+  '\t\t\t\t\t\t\t</div>\n' +
+  '\t\t\t\t\t\t</div>\n' +
+  '\t\t\t\t\t\t<div class="bottom-article-donate">\n' +
+  '\t\t\t\t\t\t\t<strong class="inline-head">\n' +
+  '                                DONEAZĂ\n' +
+  '                            </strong>\n' +
+  '\t\t\t\t\t\t\t<ul>\n' +
+  '\t\t\t\t\t\t\t\t<li>\n' +
+  '\t\t\t\t\t\t\t\t\t<a href="https://paypal.me/emisiuniAgora" target="_blank">\n' +
+  '\t\t\t\t\t\t\t\t\t\t<img src="files/images/donate-paypal-white.png" alt="Donate with PayPal">\n' +
+  '\t\t\t\t\t\t\t\t\t\t<span>\n' +
+  '                                            Susține prin\n' +
+  '\t\t\t\t\t\t\t\t\t\t\t\n' +
+  '\t\t\t\t\t\t\t\t\t\t\t<strong>PayPal</strong>\n' +
+  '\t\t\t\t\t\t\t\t\t\t</span>\n' +
+  '\t\t\t\t\t\t\t\t\t</a>\n' +
+  '\t\t\t\t\t\t\t\t</li>\n' +
+  '\t\t\t\t\t\t\t\t<li>\n' +
+  '\t\t\t\t\t\t\t\t\t<a href="https://www.patreon.com/citescAGORA" target="_blank">\n' +
+  '\t\t\t\t\t\t\t\t\t\t<img src="files/images/donate-patreon-white.png" alt="Donate with Patreon">\n' +
+  '\t\t\t\t\t\t\t\t\t\t<span>\n' +
+  '                                            Susține prin\n' +
+  '\t\t\t\t\t\t\t\t\t\t\t\n' +
+  '\t\t\t\t\t\t\t\t\t\t\t<strong>Patreon</strong>\n' +
+  '\t\t\t\t\t\t\t\t\t\t</span>\n' +
+  '\t\t\t\t\t\t\t\t\t</a>\n' +
+  '\t\t\t\t\t\t\t\t</li>\n' +
+  '\t\t\t\t\t\t\t</ul>\n' +
+  '\t\t\t\t\t\t</div>\n' +
+  '\t\t\t\t\t\t<div class="bottom-article-app">\n' +
+  '\t\t\t\t\t\t\t<strong class="inline-head">\n' +
+  '\t\t\t\t\t\t\t    Descarcă aplicația AGORA\n' +
+  '\t\t\t\t\t\t    </strong>\n' +
+  '\t\t\t\t\t\t\t<div class="flexer app-downloads">\n' +
+  '\t\t\t\t\t\t\t\t<a href="https://itunes.apple.com/md/app/agora.md/id1031313630?mt=8" target="_blank">\n' +
+  '\t\t\t\t\t\t\t\t\t<img src="files/images/appstore.png" alt="">\n' +
+  '\t\t\t\t\t\t\t\t</a>\n' +
+  '\t\t\t\t\t\t\t\t<a href="https://play.google.com/store/apps/details?id=com.sensmedia.agora&amp;hl=en" target="_blank">\n' +
+  '\t\t\t\t\t\t\t\t\t<img src="files/images/playstore.png" alt="">\n' +
+  '\t\t\t\t\t\t\t\t</a>\n' +
+  '\t\t\t\t\t\t\t</div>\n' +
+  '\t\t\t\t\t\t</div>\n' +
+  '\t\t\t\t\t</div>\n' +
+  '\t\t\t\t</article>')
